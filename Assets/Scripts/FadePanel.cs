9e9c883e9d880cf3e701827fdea4475c1dcb3157
nilo <nilold@gmail.com>
2018-09-01 15:07:45 -0300

    fade efect on splash screeen

---
+++ Assets/Scripts/FadePanel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadePanel : MonoBehaviour
+{
+
+    public float fadeTime = 3f;
+    private Color currentColor = Color.white;
+
+    Image fadePanel;
+
+    // Use this for initialization
+    void Start()
+    {
+        fadePanel = GetComponent<Image>();
+    }
+
+    private void Update()
+    {
+        if(Time.timeSinceLevelLoad < fadeTime){
+            float alphaChange = Time.deltaTime / fadeTime;
+            currentColor.a -= alphaChange;
+            fadePanel.color = currentColor;
+        } else{
+            gameObject.SetActive(false);
+        }
+
+    }
+
+}
