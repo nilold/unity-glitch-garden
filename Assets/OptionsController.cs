ba238a538e3773de62253323bf919329cac85955
nilo <nilold@gmail.com>
2018-09-01 17:03:02 -0300

    Boilerplate - Generic game framework

--- Assets/OptionsController.cs
+++ Assets/OptionsController.cs
@@ -3,26 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class OptionsController : MonoBehaviour {
+public class OptionsController : MonoBehaviour
+{
 
     [SerializeField] Slider volumeSlider;
     [SerializeField] Slider difficultySlider;
     [SerializeField] SceneLoader sceneLoader;
     MusicPlayer musicPlayer;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         musicPlayer = FindObjectOfType<MusicPlayer>();
         volumeSlider.value = PlayerPrefsManager.GetMasterVolume();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        musicPlayer.setVolume(volumeSlider.value);
-	}
+        difficultySlider.value = PlayerPrefsManager.GetDifficulty();
+    }
 
-    public void SaveAndExit(){
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ChangeVolume()
+    {
+        if (musicPlayer)
+            musicPlayer.setVolume(volumeSlider.value);
+    }
+
+    public void SetDefaults(){
+        volumeSlider.value = PlayerPrefsManager.defaultMasterVolume;
+        difficultySlider.value = PlayerPrefsManager.defaultDifficulty;
+    }
+
+
+    public void SaveAndExit()
+    {
         PlayerPrefsManager.SetMasterVolume(volumeSlider.value);
+        PlayerPrefsManager.SetDifficulty(difficultySlider.value);
 
         sceneLoader.LoadScene("01a StartMenu");
     }
--- Assets/Scripts/PlayerPrefsManager.cs
+++ Assets/Scripts/PlayerPrefsManager.cs
@@ -1,37 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class PlayerPrefsManager : MonoBehaviour {
+public class PlayerPrefsManager : MonoBehaviour
+{
 
     const string MASTER_VOLUME_KEY = "master_volume";
     const string DIFFICULTY_KEY = "difficulty";
     const string LEVEL_KEY = "level_unlocked_";
 
-    public static void SetMasterVolume(float volume){
-        if(volume >= 0f && volume <= 1f){
+    public static float defaultMasterVolume = 0.5f;
+    public static float defaultDifficulty = 2f;
+
+    public static void SetMasterVolume(float volume)
+    {
+        if (volume >= 0f && volume <= 1f)
+        {
             PlayerPrefs.SetFloat(MASTER_VOLUME_KEY, volume);
-        } else {
+        }
+        else
+        {
             Debug.LogError("Tried to set volume out of range: " + volume.ToString());
         }
     }
 
-    public static float GetMasterVolume(){
-        return PlayerPrefs.GetFloat(MASTER_VOLUME_KEY);
+    public static float GetMasterVolume()
+    {
+        if (PlayerPrefs.HasKey(MASTER_VOLUME_KEY))
+            return PlayerPrefs.GetFloat(MASTER_VOLUME_KEY);
+        return defaultMasterVolume;
     }
 
 
-    public static void UnlockLevel(int level){
-        if(level >= 0 && level < Application.levelCount){
+    public static void UnlockLevel(int level)
+    {
+        if (level >= 0 && level < Application.levelCount)
+        {
             PlayerPrefs.SetInt(LEVEL_KEY + level.ToString(), 1);
-        }else{
+        }
+        else
+        {
             Debug.LogError("Tried to unlock level not in build order");
         }
     }
 
     public static void LockLevel(int level)
     {
-        if (level >= 0 && level < Application.levelCount)
+        if (level >= 0 && level < SceneManager.sceneCountInBuildSettings)
         {
             PlayerPrefs.SetInt(LEVEL_KEY + level.ToString(), 0);
         }
@@ -41,19 +57,25 @@ public class PlayerPrefsManager : MonoBehaviour {
         }
     }
 
-    public static bool IsLevelUnlocked(int level){
+    public static bool IsLevelUnlocked(int level)
+    {
         return PlayerPrefs.GetInt(LEVEL_KEY + level.ToString()) == 1;
     }
 
-    public static void SetDifficulty(float dificulty){
-        if(dificulty >= 0f && dificulty <= 1f){
+    public static void SetDifficulty(float dificulty)
+    {
+        if (dificulty >= 1f && dificulty <= 3f)
+        {
             PlayerPrefs.SetFloat(DIFFICULTY_KEY, dificulty);
-        }else{
+        }
+        else
+        {
             Debug.LogError("Tried to set invalid value of dificulty: " + dificulty.ToString());
         }
     }
 
-    public static float GetDifficulty(){
+    public static float GetDifficulty()
+    {
         return PlayerPrefs.GetFloat(DIFFICULTY_KEY);
     }
 }
